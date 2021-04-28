f1d4c07420a368aa3766bc72f73f97bbeec37172
anshuman725 <anshuman.par@gmail.com>
2021-04-28 20:42:47 +0530

    ds-algo

---
+++ height-and-diameter-of-binary-tree.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val)
+    {
+        data=val;
+        left=NULL;
+        right=NULL;
+
+        
+    }
+
+};
+int height(Node* root)
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+
+    int lnode=height(root->left);
+    int rnode=height(root->right);
+    return max(lnode,rnode)+1;
+}
+int main()
+{
+    Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->right->left=new Node(6);
+    root->right->right=new Node(7);
+
+    cout<<height(root)<<endl;
+}
