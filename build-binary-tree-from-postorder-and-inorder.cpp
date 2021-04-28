37fc351e8ff61ce10043853be50cff6b36351d8f
anshuman725 <anshuman.par@gmail.com>
2021-04-28 16:02:20 +0530

    ds-algo

---
+++ build-binary-tree-from-postorder-and-inorder.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val)
+    {
+        data=val;
+        left=NULL;
+        right=NULL;
+
+        
+    }
+
+};
+
+int search(int inorder[],int start,int end,int val)
+{
+    for(int i=start;i<=end;i++)
+    {
+        if(inorder[i]==val)
+        {
+            return i;
+        }
+
+    }
+    return -1;
+}
+
+void inorderPrint(struct Node* root)
+{
+  if(root==NULL)
+    {
+        return;
+    }
+
+    inorderPrint(root->left);
+    cout<<root->data<<" ";
+    inorderPrint(root->right);  
+}
+
+
+Node* buildTree(int postorder[],int inorder[],int start,int end)
+{
+    if(start>end)
+    {
+        return NULL;
+    }
+    static int idx=4;
+    int val=postorder[idx];
+    idx--;
+    Node* curr =new Node(val);
+    if(start==end)
+    {
+        return curr;
+    }
+
+    int pos=search(inorder,start,end,val);
+    curr->right=buildTree(postorder,inorder,pos+1,end);
+    curr->left=buildTree(postorder,inorder,start,pos-1);
+
+    return curr;
+
+}
+
+int main()
+{
+    int postorder[]={4,2,5,3,1};
+    int inorder[]={4,2,1,5,3};
+
+    Node* root = buildTree(postorder,inorder,0,4);
+
+    inorderPrint(root);
+
+
+}
