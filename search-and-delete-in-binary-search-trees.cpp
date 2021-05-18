b25c2417fc274f338ce8709761f83a747a937da7
anshuman725 <anshuman.par@gmail.com>
2021-05-18 17:57:01 +0530

    ds-algo

--- search-and-delete-in-binary-search-trees.cpp
+++ search-and-delete-in-binary-search-trees.cpp
@@ -11,6 +11,18 @@ struct Node{
         right=NULL;
     }
 };
+void inorder(Node* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+//search
 
 Node* searchInBST(Node* root,int key)
 {
@@ -32,6 +44,49 @@ Node* searchInBST(Node* root,int key)
 
     return searchInBST(root->right,key);
 }
+//delete
+Node* inorderSucc(Node* root)
+{
+    Node* curr=root;
+    while(curr&&curr->left!=NULL)
+    {
+        curr=curr->left;
+    }
+    return curr;
+}
+
+Node* deleteInBST(Node* root,int key)
+{
+    if(key<root->data)
+    {
+        root->left=deleteInBST(root->left,key);
+    }
+    else if(key>root->data)
+    {
+        root->right=deleteInBST(root->right,key);
+    }
+    else
+    {
+        if(root->left==NULL)
+        {
+            Node* temp=root->right;
+            free(root);
+            return temp;
+        }
+        else if(root->right==NULL)
+        {
+            Node* temp=root->left;
+            free(root);
+            return temp;
+        }
+        Node* temp=inorderSucc(root->right);
+        root->data=temp->data;
+        root->right=deleteInBST(root->right,temp->data);
+    }
+    return root;
+    
+
+}
 
 int main()
 {
@@ -44,11 +99,20 @@ int main()
 
     if(searchInBST(root,10)==NULL)
     {
-        cout<<"Key Doesn't exists..." ;
+        cout<<"Key Doesn't exists..."<<endl ;
     }
     else{
-        cout<<"Key exists...";
+        cout<<"Key exists..."<<endl;
     }
+
+    inorder(root);
+    cout<<endl;
+    root=deleteInBST(root,5);
+
+    inorder(root);
+    cout<<endl;
+
+
 
 
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,7 @@
- // root->right=new Node(3);
+if(searchInBST(root,10)==NULL)
+    {
+        cout<<"Key Doesn't exists..." ;
+    }
+    else{
+        cout<<"Key exists...";
+    }
